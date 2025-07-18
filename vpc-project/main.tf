124b6fd8da03ad97eec21918ed096a4cb03e29da
Aalyankhan <aalyankhan029@gmail.com>
2025-07-18 19:29:02 +0500

    public & private in 2 AZs

--- vpc-project/main.tf
+++ vpc-project/main.tf
@@ -7,3 +7,26 @@ resource "aws_vpc" "main" {
   }
 }
 
+data "aws_availability_zone" "available" {
+}
+
+resource "aws_subnet" "public" {
+      count = 2
+      vpc_id = aws_vpc.main.id
+      cidr_block = var.public_subnet_cidr[count.index]
+      availability_zone = data.aws_availability_zone.available.names[count.index]
+      map_public_ip_on_launch = true
+      tags = {
+        "Name" = "MyPublicSubnet-${count.index + 1}"
+      }
+}
+
+resource "aws_subnet" "private" {
+      count = 2
+      vpc_id = aws_vpc.main.id
+      cidr_block = var.private_subnet_cidr[count.index]
+      availability_zone = data.aws_availability_zone.available.names[count.index]
+      tags = {
+        "Name" = "MyPrivateSubnet-${count.index + 1}"
+      }
+}
